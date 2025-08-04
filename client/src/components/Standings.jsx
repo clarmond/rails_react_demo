2b85b66ab1518706479f7ff2b829c0aa0af1f768
Chad Armond <clarmond@yahoo.com>
2025-08-04 11:07:50 -0500

    Added league logos

--- client/src/components/Standings.jsx
+++ client/src/components/Standings.jsx
@@ -5,7 +5,18 @@ const Standings = ({ data }) => {
         <>
             {data.length && data.map(section => (
                 <section key={section.sectionName}>
-                    <div className="fs-6 fw-bold pt-3 pb-1">{section.sectionName}</div>
+                    <div className="d-inline-flex align-items-center fs-6 fw-bold pt-3 pb-1">
+                        { section.sectionName.includes('American') && (
+                            <img src="images/al-logo.png" alt="American League logo" className="league-logo" />
+                        )}
+                        { section.sectionName.includes('National') && (
+                            <img src="images/nl-logo.png" alt="National League logo" className="league-logo" />
+                        )}
+                        { section.sectionName.includes('Overall') && (
+                            <img src="images/mlb-logo.png" alt="Major League Baseball logo" className="league-logo" />
+                        )}
+                        {section.sectionName}
+                    </div>
                     <StandingsTable teamData={section.data} />
                 </section>
             ))}
