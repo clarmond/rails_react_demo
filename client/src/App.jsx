9e1bf80ecb0424d4d76cd3052809cce695f8044f
Chad Armond <clarmond@yahoo.com>
2025-08-04 10:55:00 -0500

    Added loading display

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import loadData from './DataLoader';
+import Loading from './components/Loading';
 import Standings from './components/Standings';
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
@@ -8,12 +9,14 @@ function App() {
   const [ divisionStandings, setDivisionStandings ] = useState({});
   const [ leagueStandings, setLeagueStandings ] = useState({});
   const [ overallStandings, setOverallStandings ] = useState({});
+  const [ isLoading, setIsLoading ] = useState(true);
   useEffect(() => {
     async function dataLoad() {
       const { divisionData, leagueData, overallData } = await loadData();
       setDivisionStandings(divisionData);
       setLeagueStandings(leagueData);
       setOverallStandings(overallData);
+      setIsLoading(false);
     }
     dataLoad();
   }, []);
@@ -21,17 +24,21 @@ function App() {
     <>
       <div className="container py-3">
         <h1 className="fs-3 text-center">MLB Standings</h1>
-        <Tabs defaultActiveKey="division">
-          <Tab eventKey="division" title="Division">
-            <Standings data={divisionStandings} />
-          </Tab>
-          <Tab eventKey="league" title="League">
-            <Standings data={leagueStandings} />
-          </Tab>
-          <Tab eventKey="overall" title="Overall">
-            <Standings data={overallStandings} />
-          </Tab>
-        </Tabs>
+        { isLoading ? (
+          <Loading />
+        ) : (
+          <Tabs defaultActiveKey="division">
+            <Tab eventKey="division" title="Division">
+              <Standings data={divisionStandings} />
+            </Tab>
+            <Tab eventKey="league" title="League">
+              <Standings data={leagueStandings} />
+            </Tab>
+            <Tab eventKey="overall" title="Overall">
+              <Standings data={overallStandings} />
+            </Tab>
+          </Tabs>
+        ) }
       </div>
     </>
   )
